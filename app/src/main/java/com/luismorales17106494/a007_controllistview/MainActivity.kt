43e8eae3a48627423243151a763103d968c82bcc
Edu-Lu <moralesluis17106494@gmail.com>
2022-06-28 14:09:47 -0500

    Ejercicio ControlListView Resuelto

--- app/src/main/java/com/luismorales17106494/a007_controllistview/MainActivity.kt
+++ app/src/main/java/com/luismorales17106494/a007_controllistview/MainActivity.kt
@@ -2,6 +2,8 @@ package com.luismorales17106494.a007_controllistview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.luismorales17106494.a007_controllistview.databinding.ActivityMainBinding
 
 /*
     7 - Control ListView (con una lista de String)
@@ -17,11 +19,31 @@ import android.os.Bundle
     El control ListView se encuentra en la pestaña "Containers".
 
 
+    Problema
+    Disponer un ListView con los nombres de paises de sudamérica. Cuando se seleccione un
+    país mostrar en un TextView la cantidad de habitantes del país seleccionado.
  */
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val Listapaises = arrayOf( "Argentina", "Chile", "Paraguay", "Bolivia", "Peru", "Ecuador", "Brasil", "Colombia", "Venezuela", "Uruguay")
+        var habitantes = arrayOf(40_000_000, 17_000_000, 6_500_000, 10_000_000, 30_000_000, 14_000_000, 183_000_000, 44_000_000, 31_000_000, 3_500_000)
+
+
+        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,Listapaises)
+        binding.lvListaPaises.adapter = adapter
+
+
+        binding.lvListaPaises.setOnItemClickListener{ adapterView, view , item, l ->
+
+            binding.tvNumeroDeHabitantes.text = "${Listapaises[item]} : ${habitantes[item]} habitantes"
+
+        }
+
     }
 }
